f446942317c210db47e0449bc05d22833027e733
bfuller7141 <bfuller@tripassdesign.com>
2024-06-20 13:27:26 -0700

    edits

--- clean-code.py
+++ clean-code.py
@@ -13,14 +13,17 @@ def clean_html(html_content):
     # Add lang attribute if missing
     if not soup.html.has_attr('lang'):
         soup.html['lang'] = 'en'
+        print("Added lang attribute to HTML tag.")
 
     # Replace favicon and apple-touch-icon link elements
     for link in soup.find_all('link'):
         if link.get('href'):
             if "{{ settings.site.favicon" in link['href']:
                 link['href'] = "{{ settings.site.favicon | default: 'images/favicon.ico' }}"
+                print(f"Updated favicon link: {link}")
             if "{{ settings.site.apple_touch_icon" in link['href']:
                 link['href'] = "{{ settings.site.apple_touch_icon | default: 'images/favicon.png' }}"
+                print(f"Updated apple-touch-icon link: {link}")
 
     # Preserve specific inline formatting for title-small divs
     for div in soup.find_all('div', class_='title-small'):
@@ -28,6 +31,7 @@ def clean_html(html_content):
         new_div = soup.new_tag('div', **{'class': 'title-small'})
         new_div.append(BeautifulSoup(new_content, 'html.parser'))
         div.replace_with(new_div)
+        print(f"Updated div with class 'title-small': {new_div}")
 
     return str(soup)
 
